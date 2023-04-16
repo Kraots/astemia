ee2597949d2ba08307be90f4158511da791f7270
Kraots <nagit.robertmessi@gmail.com>
2023-04-16 22:47:21 +0300

    lil more fixes

--- utils/paginator.py
+++ utils/paginator.py
@@ -238,7 +238,7 @@ class FieldPageSource(menus.ListPageSource):
 
     def __init__(self, entries, *, per_page=12):
         super().__init__(entries, per_page=per_page)
-        self.embed = disnake.Embed(colour=disnake.Colour.blurple())
+        self.embed = disnake.Embed(colour=disnake.Colour.red())
 
     async def format_page(self, menu, entries):
         self.embed.clear_fields()
@@ -341,7 +341,7 @@ class SimplePages(RoboPages):
             ctx=ctx, compact=compact
         )
         if color is None:
-            color = disnake.Color.blurple()
+            color = disnake.Color.red()
         self.embed = disnake.Embed(colour=color)
 
 
@@ -370,7 +370,7 @@ class RawSimplePages(RoboPages):
     def __init__(self, ctx, entries, *, per_page=5, color=None, compact=False):
         super().__init__(RawSimplePageSource(entries, per_page=per_page), ctx=ctx, compact=compact)
         if color is None:
-            color = disnake.Color.blurple()
+            color = disnake.Color.red()
         self.embed = disnake.Embed(colour=color)
 
 
